d3f9836f260b1814c6df926544a7c013556db65e
celeste emery <88988526+c-emery-0@users.noreply.github.com>
2021-08-24 11:58:55 -0500

    Created programs from pg. 47-48 (1.1, 1.2, 1.3)

---
+++ chapter 1&2/Test.java
@@ -0,0 +1,14 @@
+
+/**
+ * pg. 47; 1.1, 1.2
+ *
+ * @author (celeste emery)
+ * @version (8/24/2021)
+ */
+public class Test
+{
+    public static void main (String[] args)
+    {
+        System.out.println ("An Emergency Broadcast");
+    }
+}   
---
+++ chapter 1&2/pg48_13.java
@@ -0,0 +1,18 @@
+
+/**
+ * displays a bunch of my own personal information. (pg. 48, 1.3)
+ *
+ * @author (celeste emery)
+ * @version (8/24/2021)
+ */
+public class pg48_13
+{
+    public static void main (String[] args)
+    {
+        System.out.println ("Name: Celeste Emery");
+        System.out.println ("Birthday: September 15th 2005");
+        System.out.println ("Hobbies: Drawing");
+        System.out.println ("Favorite book: On a Sunbeam");
+        System.out.println ("Favorite movie: Space Sweepers");
+    }
+}   
